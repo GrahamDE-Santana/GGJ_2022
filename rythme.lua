e76b9a7e87e7ac87e9b453a73b3acec357346600
mayol_l <lucas1.mayol@epitech.eu>
2022-01-29 15:07:04 +0100

    audio edit

--- rythme.lua
+++ rythme.lua
@@ -1,5 +1,9 @@
 RYTHME = {}
 
+function math.Clamp(val, min, max)
+    return math.min(math.max(val, min), max)
+end
+
 function  RYTHME:new(tempo, nb)
   local obj = {}
   setmetatable(obj, self)
@@ -13,17 +17,23 @@ function  RYTHME:new(tempo, nb)
   obj.data[1] = {false, false, false, false}
   obj.u = (60. / obj.tempo)
 
+  obj.x = 1
+  obj.y = 1
+  obj.loop = true
+  obj.barre = false
   return (obj)
 end
 
-function RYTHME:getCurrentValue()
-  local x = 1 + math.floor(self.t / self.u)
+function RYTHME:getXY()
+  local x = 1 + math.floor((self.t) / self.u)
   local y = 1 + math.floor(math.fmod((self.t / self.u) + 1, x) * 4)
 
-  --print("dt : ", self.t)
-  --print("x : ", x, self.u * self.t)
-  --print("y : ", y, math.fmod(self.u * self.t, self.u))
-  --print("")
+  return x, y
+end
+
+function RYTHME:getCurrentValue()
+  local x, y = self:getXY()
+
   return ((self.data[x])[y])
 end
 
@@ -48,21 +58,32 @@ end
 
 
 function RYTHME:update(dt)
+  self.barre = false
   self.t = dt + self.t
   if self.t >= self.u * self.nb then
-    self.t = 0
+    if self.loop == true then
+      self.t = 0
+    else
+      self.t = self.u * self.nb
+    end
+  end
+  local x, y = self:getXY()
+  if x ~= self.x or y ~= self.y then
+    if x ~= self.x then self.barre = true end
+    self.x = x
+    self.y = y
   end
 end
 
-function RYTHME:edit(dt, v)
-  self.t = dt + self.t
-  if self.t >= self.nb * self.tempo then
-    self.t = self.nb * self.tempo
-  end
-  if v ~= nil then
-    local x, y = self:getTable()
-    self.data[x][y] = v
-  end
+function RYTHME:edit(v)
+  local tmp = self.t
+  self.t = self.t + (self.u * 0.25)
+  local x, y = self:getXY()
+  x = math.Clamp(x, 1, self.nb)
+  y = math.Clamp(y, 1, self.nb)
+
+  self.data[x][y] = v
+  self.t = tmp
 end
 
 function RYTHME:endEdit()
