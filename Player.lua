416bff64bf21cf6c0518e380e7bfe2581a8bb08b
victorvdv <victor.vindevogel@epitech.eu>
2022-01-29 18:52:32 +0100

    (+) assets

--- Player.lua
+++ Player.lua
@@ -2,6 +2,9 @@
 
 --Player = Class('Player')
 
+local Y_SCALING = 42
+local X_SCALING = 44
+
 PLAYER = {}
 PLAYERS = {}
 
@@ -30,18 +33,22 @@ function PLAYER:new(key)
    if (PLAYER:getSize() == 0) then
       obj.animation = PLAYER:newAnimation(love.graphics.newImage("assets/mask_idle32x32.png"), 32, 32, 1)
       obj.color = {255, 0, 0}
+      obj.scale = {X_SCALING / 32 * 2, Y_SCALING / 32 * 2}
       obj.pos = {20, 20}
    elseif (PLAYER:getSize() == 1) then
       obj.animation = PLAYER:newAnimation(love.graphics.newImage("assets/virual_idle32x32.png"), 32, 32, 1)
       obj.color = {0, 255, 0}
+      obj.scale = {X_SCALING / 32 * 2, Y_SCALING / 32 * 2}
       obj.pos = {20, 480}
    elseif (PLAYER:getSize() == 2) then
       obj.animation = PLAYER:newAnimation(love.graphics.newImage("assets/idle_bunny34x44.png"), 34, 44, 1)
       obj.color = {0, 0, 255}
+      obj.scale = {X_SCALING / 34 * 2, Y_SCALING / 44 * 2}
       obj.pos = {680, 20}
    else
       obj.animation = PLAYER:newAnimation(love.graphics.newImage("assets/plant_idle44x42.png"), 44, 42, 1)
       obj.color = {255, 255, 0}
+      obj.scale = {2, 2}
       obj.pos = {680, 480}
    end
    obj.key = key
@@ -62,7 +69,8 @@ return {
    draw = function()
       for k, val in pairs(PLAYERS) do
          local spriteNum = math.floor(val.animation.currentTime / val.animation.duration * #val.animation.quads) + 1
-         love.graphics.draw(val.animation.spriteSheet, val.animation.quads[spriteNum], val.pos[1], val.pos[2], 0, 2, 2)
+         love.graphics.draw(val.animation.spriteSheet, val.animation.quads[spriteNum],
+                            val.pos[1], val.pos[2], 0, val.scale[1], val.scale[2])
       end
    end,
    update = function(dt)
