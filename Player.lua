660fa3381c5ad38ff30af900221514cbaaa947cc
victorvdv <victor.vindevogel@epitech.eu>
2022-01-29 16:24:18 +0100

    (*) Player

--- Player.lua
+++ Player.lua
@@ -10,7 +10,21 @@ function PLAYER:new(key)
    setmetatable(obj, self)
    self.__index = self
 
+   if (PLAYER:getSize() == 0) then
+      obj.color = {255, 0, 0}
+      obj.pos = {20, 20}
+   elseif (PLAYER:getSize() == 1) then
+      obj.color = {0, 255, 0}
+      obj.pos = {20, 480}
+   elseif (PLAYER:getSize() == 2) then
+      obj.color = {0, 0, 255}
+      obj.pos = {680, 20}
+   else
+      obj.color = {255, 255, 0}
+      obj.pos = {680, 480}
+   end
    obj.key = key
+   obj.life = 3
    obj.pressed = false
    return (obj)
 end
@@ -23,7 +37,18 @@ function PLAYER:getSize()
    return len
 end
 
+function PLAYER:update()
+   --
+end
+
 return {
+   draw = function()
+      for k, val in pairs(PLAYERS) do
+         love.graphics.setColor(val.color[1], val.color[2], val.color[3])
+         love.graphics.rectangle("fill", val.pos[1], val.pos[2], 100, 100)
+      end
+      love.graphics.setColor(255, 255, 255)
+   end,
    playerPressDown = function(key)
       for k, val in pairs(PLAYERS) do
          if (val.key == key) then
